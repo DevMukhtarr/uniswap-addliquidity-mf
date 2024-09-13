e9471c3390fe87e665545287bd5308df8a532447
Mukhtar Abdulfatai <mukhtarabdulfatai@gmail.com>
2024-09-13 16:29:47 +0100

    added impersonatedSigner to Dai contract

--- scripts/mainnet-forking.ts
+++ scripts/mainnet-forking.ts
@@ -6,14 +6,16 @@ async function main() {
     const USDC_ADDRESS = "0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48";
     const DAI_ADDRESS = "0x6B175474E89094C44Da98b954EedeAC495271d0F";
 
-    const to = "0xbEbc44782C7dB0a1A60Cb6fe97d0b483032FF1C7"
+    const to = "0xf584F8728B874a6a5c7A8d4d387C9aae9172D621"
 
     await helpers.impersonateAccount(to);
     const impersonatedSigner = await ethers.getSigner(to);
 
 
-    const amountADesired = ethers.parseUnits("70", 18);
-    const amountBDesired = ethers.parseUnits("70", 18);
+    const amountADesired = ethers.parseUnits("1000", 6);
+    const amountBDesired = ethers.parseUnits("1000", 18);
+    const amountAMin = ethers.parseUnits("20", 6);
+    const amountBMin = ethers.parseUnits("20", 18);
     
     const USDC_CONTRACT = await ethers.getContractAt("IERC20", USDC_ADDRESS, impersonatedSigner);
     const DAI_CONTRACT = await ethers.getContractAt("IERC20", DAI_ADDRESS, impersonatedSigner);
@@ -23,8 +25,6 @@ async function main() {
     await USDC_CONTRACT.approve(ROUTER, amountADesired);
     await DAI_CONTRACT.approve(ROUTER, amountBDesired);
     
-    const amountAMin = ethers.parseUnits("100", 6);
-    const amountBMin = ethers.parseUnits("100", 6);
 
     const deadline = Math.floor(Date.now() / 1000) + (60 * 10);
 
@@ -36,8 +36,8 @@ async function main() {
 
     
     await ROUTER.addLiquidity(
-        USDC_ADDRESS,
-        DAI_ADDRESS,
+        USDC_CONTRACT,
+        DAI_CONTRACT,
         amountADesired,
         amountBDesired,
         amountAMin,
